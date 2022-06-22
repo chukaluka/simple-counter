ccb9861c6b3d5aa078cf8a639037477f91e38c9c
Chukwuka Okonkwo David <chukwukaokonkwo99@gmail.com>
2022-06-22 10:17:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `Clicked ${count} times`
+    })
+
+    const increment = () => {
+        setCount( count + 1)
+    }; 
+
+    return (
+        <div>
+            <h2>Counter app</h2>
+            <button onClick={increment}> Clicked {count} times</button>
+       </div>
+    );
+
+};
+
+export default App;
